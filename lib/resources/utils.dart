1c75d955847abace1e7c436d97031d710bc0e969
Mahadhi Hassan Chowdhury <mhcsunny@gmail.com>
2023-05-20 01:03:38 +0600

    folders created

---
+++ lib/resources/utils.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Utils{
+
+
+  Widget sizeVer(double height) {
+    return SizedBox(height: height,);
+  }
+
+  Widget sizeHor(double width) {
+    return SizedBox(width: width);
+  }
+
+}
